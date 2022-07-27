e230aa442e5f94cf009d0f580219b36c06478b8d
LapoCorti <l.corti5@studenti.unipi.it>
2022-07-27 15:22:37 +0200

    Plot delle 4 particelle sullo stesso grafico e con stesse scale su assi

--- Graphs_simulation.jl
+++ Graphs_simulation.jl
@@ -4,16 +4,17 @@
 # grafico vado a plottare 4-5 traiettorie diverse della stessa particella. 
 
 
-include("ActiveBrownianParticles2.jl");
+include("MyActiveBrownianParticle.jl");
 
 using BenchmarkTools
 using Plots 
 using Distances 
 
-## PARTIAMO DAL PRIMO GRAFICO PER UNA PARTICELLA DI RAGGIO 1.5MICRON E V=0 ##
+## PARTIAMO DAL PRIMO GRAFICO PER UNA PARTICELLA DI RAGGIO 0.5MICRON E V=0 ##
 
-#inizializzazione particella (R=1.5e-6m , v=0m/s)
-abp1 = initABP( (0.0, 0.0, 0.0), 1.5, 0.0 );
+#inizializzazione particella (R=0.5e-6m , v=0m/s)
+abp1 = initABP( (0.0, 0.0, 0.0), 0.5, 0.0 );
+
 
 #creo il primo grafico grafico 
 N = 100000
@@ -31,46 +32,43 @@ y = [pi[2] for pi in p]
 # assume tutti i valori dentro p (cioè parte da p)
 
 #plotto in x y
-plot1 = plot_trajectory(x,y)
+
+plot(x,y, range=[-100,75], title = "ActiveParticle1 (R=0.5µm, v=0µm/s)", aspect_ratio= :equal,legend=false) 
+#scatter!([x[end]],[y[end]], legend=false)
+xlabel!("x [μm]")
+ylabel!("y [μm]")
+
 
 #realizzo più grafici nello stesso plot (subplot) 
 # (vedere se riesco a migliorare questa parte)
 p, t = trajectory( abp1, N);
 x = [pi[1] for pi in p]
 y = [pi[2] for pi in p]
-plot2 = plot_trajectory(x,y)
+plot!(x,y,range=[-100,75],legend=false,aspect_ratio= :equal)
+#scatter!([x[end]],[y[end]], legend=false)
 
 p, t = trajectory( abp1, N);
 x = [pi[1] for pi in p]
 y = [pi[2] for pi in p]
-plot3 = plot_trajectory(x,y)
+plot!(x,y,range=[-100,75], legend=false,aspect_ratio= :equal)
+#scatter!([x[end]],[y[end]], legend=false)
 
 p, t = trajectory( abp1, N);
 x = [pi[1] for pi in p]
 y = [pi[2] for pi in p]
-plot4 = plot_trajectory(x,y)
-
-p, t = trajectory( abp1, N);
-x = [pi[1] for pi in p]
-y = [pi[2] for pi in p]
-plot5 = plot_trajectory(x,y)
-
-p, t = trajectory( abp1, N);
-x = [pi[1] for pi in p]
-y = [pi[2] for pi in p]
-plot6 = plot_trajectory(x,y)
+plot!(x,y,range=[-100,75],legend=false,aspect_ratio= :equal)
+#scatter!([x[end]],[y[end]], legend=false)
     
-#subplot e metto i 6 grafici in un unico grafico
-plot(plot1, plot2 , plot3, plot4, plot5, plot6 ; layout = 6, title = ["Trajectory 1" "Trajectory 2" "Trajectory 3" "Trajectory 4"  "Trajectory 5" "Trajectory 6"], plot_title = "ActiveParticle1 (R=1.5µm, v=0µm/s)", plot_titlevspan=0.1)
-
- 
+#subplot e metto i 4 grafici in un unico grafico facendo subplot
+#plot(plot1, plot2 , plot3, plot4; layout = 4, title = ["Trajectory 1" "Trajectory 2" "Trajectory 3" "Trajectory 4" ], plot_title = "ActiveParticle1 (R=0.5µm, v=0µm/s)", plot_titlevspan=0.1)
 
 
 
-## RIPETIAMO PER UNA PARTICELLA DI RAGGIO 1.5MICRON E V=10µm/s ##
 
-#inizializzazione particella (R=1.5e-6m , v=10µm/s)
-abp1 = initABP( (0.0, 0.0, 0.0), 1.5, 10.0 );
+## RIPETIAMO PER UNA PARTICELLA DI RAGGIO 0.5MICRON E V=5µm/s ##
+
+#inizializzazione particella (R=0.5e-6m , v=5µm/s)
+abp1 = initABP( (0.0, 0.0, 0.0), 0.5, 5.0 );
 
 #creo il primo grafico grafico 
 N = 100000
@@ -88,46 +86,45 @@ y = [pi[2] for pi in p]
 # assume tutti i valori dentro p (cioè parte da p)
 
 #plotto in x y
-plot1 = plot_trajectory(x,y)
+plot(x,y,range=[-100,75], title = "ActiveParticle2 (R=0.5µm, v=5µm/s)", aspect_ratio= :equal,legend=false) 
+#scatter!([x[end]],[y[end]], legend=false)
+xlabel!("x [μm]")
+ylabel!("y [μm]")
 
 #realizzo più grafici nello stesso plot (subplot) 
 # (vedere se riesco a migliorare questa parte)
 p, t = trajectory( abp1, N);
 x = [pi[1] for pi in p]
 y = [pi[2] for pi in p]
-plot2 = plot_trajectory(x,y)
+plot!(x,y,range=[-100,75],aspect_ratio= :equal,legend=false)
+#scatter!([x[end]],[y[end]], legend=false)
+
 
 p, t = trajectory( abp1, N);
 x = [pi[1] for pi in p]
 y = [pi[2] for pi in p]
-plot3 = plot_trajectory(x,y)
+plot!(x,y,range=[-100,75],aspect_ratio= :equal,legend=false)
+#scatter!([x[end]],[y[end]], legend=false)
+
 
 p, t = trajectory( abp1, N);
 x = [pi[1] for pi in p]
 y = [pi[2] for pi in p]
-plot4 = plot_trajectory(x,y)
+plot!(x,y,range=[-100,75],aspect_ratio= :equal,legend=false)
+#scatter!([x[end]],[y[end]], legend=false)
 
-p, t = trajectory( abp1, N);
-x = [pi[1] for pi in p]
-y = [pi[2] for pi in p]
-plot5 = plot_trajectory(x,y)
-
-p, t = trajectory( abp1, N);
-x = [pi[1] for pi in p]
-y = [pi[2] for pi in p]
-plot6 = plot_trajectory(x,y)
     
-#subplot e metto i 6 grafici in un unico grafico
-plot(plot1, plot2 , plot3, plot4, plot5, plot6 ; layout = 6, title = ["Trajectory 1" "Trajectory 2" "Trajectory 3" "Trajectory 4"  "Trajectory 5" "Trajectory 6"], plot_title = "ActiveParticle2 (R=1.5µm, v=10µm/s)", plot_titlevspan=0.1)
+#subplot e metto i 4 grafici in un unico grafico
+#plot(plot1, plot2 , plot3, plot4; layout = 4, title = ["Trajectory 1" "Trajectory 2" "Trajectory 3" "Trajectory 4" ], plot_title = "ActiveParticle2 (R=0.5µm, v=5µm/s)", plot_titlevspan=0.1)
 
 
 
 
 
-## RIPETIAMO PER UNA PARTICELLA DI RAGGIO 3µm E V=0µm/s ##
+## RIPETIAMO PER UNA PARTICELLA DI RAGGIO 2.5µm E v=0µm/s ##
 
-#inizializzazione particella (R=3e-6m , v=0µm/s)
-abp1 = initABP( (0.0, 0.0, 0.0), 3.0, 0.0 );
+#inizializzazione particella (R=2.5e-6m , v=0µm/s)
+abp1 = initABP( (0.0, 0.0, 0.0), 2.5, 0.0 );
 
 #creo il primo grafico grafico 
 N = 100000
@@ -145,47 +142,43 @@ y = [pi[2] for pi in p]
 # assume tutti i valori dentro p (cioè parte da p)
 
 #plotto in x y
-plot1 = plot_trajectory(x,y)
+plot(x,y, xlim=[-25,200],ylim=[-100,100], title = "ActiveParticle3 (R=2.5µm, v=0µm/s)", aspect_ratio= :equal) 
+#scatter!([x[end]],[y[end]], legend=false)
+xlabel!("x [μm]")
+ylabel!("y [μm]")
 
 #realizzo più grafici nello stesso plot (subplot) 
 # (vedere se riesco a migliorare questa parte)
 p, t = trajectory( abp1, N);
 x = [pi[1] for pi in p]
 y = [pi[2] for pi in p]
-plot2 = plot_trajectory(x,y)
+plot!(x,y,xlim=[-25,200],ylim=[-100,100],legend=false,aspect_ratio= :equal)
+#scatter!([x[end]],[y[end]], legend=false)
 
 p, t = trajectory( abp1, N);
 x = [pi[1] for pi in p]
 y = [pi[2] for pi in p]
-plot3 = plot_trajectory(x,y)
+plot!(x,y,xlim=[-25,200],ylim=[-100,100],legend=false,aspect_ratio= :equal)
+#scatter!([x[end]],[y[end]], legend=false)
 
 p, t = trajectory( abp1, N);
 x = [pi[1] for pi in p]
 y = [pi[2] for pi in p]
-plot4 = plot_trajectory(x,y)
-
-p, t = trajectory( abp1, N);
-x = [pi[1] for pi in p]
-y = [pi[2] for pi in p]
-plot5 = plot_trajectory(x,y)
-
-p, t = trajectory( abp1, N);
-x = [pi[1] for pi in p]
-y = [pi[2] for pi in p]
-plot6 = plot_trajectory(x,y)
+plot!(x,y,xlim=[-25,200],ylim=[-100,100],legend=false,aspect_ratio= :equal)
+#scatter!([x[end]],[y[end]], legend=false)
     
-#subplot e metto i 6 grafici in un unico grafico
-plot(plot1, plot2 , plot3, plot4, plot5, plot6 ; layout = 6, title = ["Trajectory 1" "Trajectory 2" "Trajectory 3" "Trajectory 4"  "Trajectory 5" "Trajectory 6"], plot_title = "ActiveParticle3 (R=3µm, v=0µm/s)", plot_titlevspan=0.1)
+#subplot e metto i 4 grafici in un unico grafico
+#plot(plot1, plot2 , plot3, plot4; layout = 4, title = ["Trajectory 1" "Trajectory 2" "Trajectory 3" "Trajectory 4"], plot_title = "ActiveParticle3 (R=2.5µm, v=0µm/s)", plot_titlevspan=0.1)
 
 
 
 
 
 
-## RIPETIAMO PER UNA PARTICELLA DI RAGGIO 1.5MICRON E V=10µm/s ##
+## RIPETIAMO PER UNA PARTICELLA DI RAGGIO 2.5MICRON E v=5µm/s ##
 
-#inizializzazione particella (R=1.5e-6m , v=10µm/s)
-abp1 = initABP( (0.0, 0.0, 0.0), 1.5, 10.0 );
+#inizializzazione particella (R=2.5e-6m , v=5µm/s)
+abp1 = initABP( (0.0, 0.0, 0.0), 2.5, 5.0 );
 
 #creo il primo grafico grafico 
 N = 100000
@@ -203,94 +196,35 @@ y = [pi[2] for pi in p]
 # assume tutti i valori dentro p (cioè parte da p)
 
 #plotto in x y
-plot1 = plot_trajectory(x,y)
+plot(x,y,xlim=[-25,200],ylim=[-100,100], title = "ActiveParticle4 (R=2.5µm, v=5µm/s)", aspect_ratio= :equal) 
+#scatter!([x[end]],[y[end]], legend=false)
+xlabel!("x [μm]")
+ylabel!("y [μm]")
 
 #realizzo più grafici nello stesso plot (subplot) 
 # (vedere se riesco a migliorare questa parte)
 p, t = trajectory( abp1, N);
 x = [pi[1] for pi in p]
 y = [pi[2] for pi in p]
-plot2 = plot_trajectory(x,y)
+plot!(x,y,xlim=[-25,200],ylim=[-100,100],legend=false,aspect_ratio= :equal)
+#scatter!([x[end]],[y[end]], legend=false)
 
 p, t = trajectory( abp1, N);
 x = [pi[1] for pi in p]
 y = [pi[2] for pi in p]
-plot3 = plot_trajectory(x,y)
+plot!(x,y,xlim=[-25,200],ylim=[-100,100],legend=false,aspect_ratio= :equal)
+#scatter!([x[end]],[y[end]], legend=false)
 
 p, t = trajectory( abp1, N);
 x = [pi[1] for pi in p]
 y = [pi[2] for pi in p]
-plot4 = plot_trajectory(x,y)
-
-p, t = trajectory( abp1, N);
-x = [pi[1] for pi in p]
-y = [pi[2] for pi in p]
-plot5 = plot_trajectory(x,y)
-
-p, t = trajectory( abp1, N);
-x = [pi[1] for pi in p]
-y = [pi[2] for pi in p]
-plot6 = plot_trajectory(x,y)
+plot!(x,y,xlim=[-25,200],ylim=[-100,100],legend=false,aspect_ratio= :equal)
+#scatter!([x[end]],[y[end]], legend=false)
     
-#subplot e metto i 6 grafici in un unico grafico
-plot(plot1, plot2 , plot3, plot4, plot5, plot6 ; layout = 6, title = ["Trajectory 1" "Trajectory 2" "Trajectory 3" "Trajectory 4"  "Trajectory 5" "Trajectory 6"], plot_title = "ActiveParticle2 (R=1.5µm, v=10µm/s)", plot_titlevspan=0.1)
+#subplot e metto i 4 grafici in un unico grafico
+#plot(plot1, plot2 , plot3, plot4; layout = 4, title = ["Trajectory 1" "Trajectory 2" "Trajectory 3" "Trajectory 4"], plot_title = "ActiveParticle4 (R=2.5µm, v=5µm/s)", plot_titlevspan=0.1)
 
 
-
-
-
-## RIPETIAMO PER UNA PARTICELLA DI RAGGIO 3µm E V=10µm/s ##
-
-#inizializzazione particella (R=3e-6m , v=10µm/s)
-abp1 = initABP( (0.0, 0.0, 0.0), 3.0, 10.0 );
-
-#creo il primo grafico grafico 
-N = 100000
-p, t = trajectory( abp1, N);
-
-#NOTA: p è un vettore di tuple, ovvero è un vettore con N elementi 
-# e ciascun elemento è una tupla (x,y, teta)
-
-#creo i vettori degli assi 
-x = [pi[1] for pi in p]
-y = [pi[2] for pi in p]
-
-#NOTA: questo comando mi permette di creare un vettore i cui elementi 
-# sono dati dai primi elementi di pi dove pi 
-# assume tutti i valori dentro p (cioè parte da p)
-
-#plotto in x y
-plot1 = plot_trajectory(x,y)
-
-#realizzo più grafici nello stesso plot (subplot) 
-# (vedere se riesco a migliorare questa parte)
-p, t = trajectory( abp1, N);
-x = [pi[1] for pi in p]
-y = [pi[2] for pi in p]
-plot2 = plot_trajectory(x,y)
-
-p, t = trajectory( abp1, N);
-x = [pi[1] for pi in p]
-y = [pi[2] for pi in p]
-plot3 = plot_trajectory(x,y)
-
-p, t = trajectory( abp1, N);
-x = [pi[1] for pi in p]
-y = [pi[2] for pi in p]
-plot4 = plot_trajectory(x,y)
-
-p, t = trajectory( abp1, N);
-x = [pi[1] for pi in p]
-y = [pi[2] for pi in p]
-plot5 = plot_trajectory(x,y)
-
-p, t = trajectory( abp1, N);
-x = [pi[1] for pi in p]
-y = [pi[2] for pi in p]
-plot6 = plot_trajectory(x,y)
-    
-#subplot e metto i 6 grafici in un unico grafico
-plot(plot1, plot2 , plot3, plot4, plot5, plot6 ; layout = 6, title = ["Trajectory 1" "Trajectory 2" "Trajectory 3" "Trajectory 4"  "Trajectory 5" "Trajectory 6"], plot_title = "ActiveParticle4 (R=3µm, v=10µm/s)", plot_titlevspan=0.1)
 
 
 
